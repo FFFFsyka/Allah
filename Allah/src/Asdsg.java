048d38a1f282f6b806bc4cc8a9ecc85b086c5948
student <mcvdoc1@gmail.com>
2018-10-25 19:22:07 +0300

    ыы сама сделала

--- Allah/src/Asdsg.java
+++ Allah/src/Asdsg.java
@@ -1,7 +1,34 @@
-public class Asdsg {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Asdsg
+ {
 
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {  Scanner in = new Scanner(System.in);
+        int a=in.nextInt();
+        int b = in.nextInt();
+        ArrayList<Integer> as = new ArrayList<>();
+        ArrayList<Integer> bs = new ArrayList<>();
+        for(int i=1;i<=a; i++){
+            if(a % i==0) {
+                as.add(i);
+            }
+        }
+        for(int j=1;j<=b;j++){
+            if((b % j == 0)&&(!as.contains(j))){
+                as.add(j);
+
+            }
+
+        }
+        System.out.println(as);
+        System.out.println(as.size());
+
+
+
+
+
+
     }
 }
